55957eca3482314978758ee7a6bdaefcd420f11c
abc-abhaya <162488354+abc-abhaya@users.noreply.github.com>
2024-07-21 21:36:11 +0545

    Tab functionality added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,18 @@
 import itClubImg from '../assets/it_club.png';
 import setFavicon from './favicon';
+import openTab from './openTab';
 
 const { log } = console;
 log('Namaste Nepal');
+const tabBtns = document.querySelectorAll('button.tab-button');
+tabBtns.forEach((btn) => {
+	btn.addEventListener('click', (e) => {
+		openTab(btn.getAttribute('data-tab'));
+		
+	});
+});
+console.log(tabBtns);
 
-div.append(h1, img);
-document.body.appendChild(div);
+openTab();
+
 setFavicon(itClubImg, 'png');
