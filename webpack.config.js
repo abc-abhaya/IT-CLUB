0c7863853aefd8a8edb537aeebdf70272c30c29f
abc-abhaya <162488354+abc-abhaya@users.noreply.github.com>
2024-07-21 21:00:35 +0545

    header styling

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,13 @@
 module.exports = {
 	entry: {
-        // script
+		// script
 		main: './src/scripts/main.js',
-        // styles
-		styles: ['./src/styles/style.css'],
+		// styles
+		styles: [
+			'./src/styles/style.css',
+			'./src/styles/main-grid.css',
+			'./src/styles/header/ribbon.css',
+			'./src/styles/header/header.css',
+		],
 	},
 };
