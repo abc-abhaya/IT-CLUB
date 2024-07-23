5b433ddac7dbeee4a41cb36d313aa7c83aa28805
abc-abhaya <162488354+abc-abhaya@users.noreply.github.com>
2024-07-23 19:24:03 +0545

    followers effect

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,7 @@ import itClubImg from '../assets/it_club.png';
 import setFavicon from './favicon';
 import openTab from './openTab';
 import typeWriter from './welcomeEffect';
+import followerEffect from './followerEffect';
 
 const { log } = console;
 log('Namaste Nepal');
@@ -17,6 +18,7 @@ window.onload = () => {
 	openTab('home');
 };
 
+followerEffect();
 typeWriter();
 
 setFavicon(itClubImg, 'png');
